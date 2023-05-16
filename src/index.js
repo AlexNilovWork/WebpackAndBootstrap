1542badd68a5ef136f90d010da66a3136aebb45d
AlexNilovWork <anilov.work@gmail.com>
2023-05-16 20:18:55 +0300

    del babel.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import * as $ from 'jquery';
 import * as bootstrap from 'bootstrap';
 import BootstrapColorPalette from './js/bscolpal';
 import './styles/style.scss';
-import './babel.js';
+//import './babel.js';
 const myPal = new BootstrapColorPalette('#example');
