8acfd79ac72aac0779c68d75ecfaf0f65f5c39b0
AlexNilovWork <anilov.work@gmail.com>
2023-05-16 20:09:11 +0300

    edit html

--- src/js/bscolpal.js
+++ src/js/bscolpal.js
@@ -2,7 +2,7 @@ import * as $ from 'jquery';
 export default class BootstrapColorPalette {
   constructor(selector) {
     this.selector = selector;
-    let pal = `<h2>Yes</h2>`;
+    let pal = `<h2>Yes Yes</h2>`;
     $(this.selector).prepend(pal);
   }
 }
