97aa156a1c5129caa6e8047b0fc6d09acc00ace0
AlexNilovWork <anilov.work@gmail.com>
2023-05-25 10:48:50 +0300

    new version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as $ from 'jquery';
 import * as bootstrap from 'bootstrap';
-import BootstrapColorPalette from './js/bscolpal';
+import BootstrapColorPalette from 'color-palette-for-users';
 import './styles/style.scss';
 //import './babel.js';
 const myPal = new BootstrapColorPalette('#example');
